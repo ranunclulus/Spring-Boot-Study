65172e1e8e4908a0a2454d76ddb5c02e5cde4302
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-12-14 18:13:46 +0900

    feat: 회원 서비스 테스트

--- hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
@@ -2,13 +2,17 @@ package com.example.hellospring.service;
 
 import com.example.hellospring.domain.Member;
 import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 의존성 주입 가능하게 변경
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /*
     회원 가입
