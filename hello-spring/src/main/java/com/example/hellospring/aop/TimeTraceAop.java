bb6f2d0e27342f4c1b80c3f8d950935849f90027
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-07-19 15:41:04 +0900

    feat: spring aop

---
+++ hello-spring/src/main/java/com/example/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,22 @@
+package com.example.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class TimeTraceAop {
+
+    @Around("execution(* example.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            System.out.println("END: " + joinPoint.toString() + " TIME: " + (end - start) + "ms");
+        }
+    }
+}
