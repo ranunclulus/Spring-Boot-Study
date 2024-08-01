029b1ce8aafe147d404236a4ad5982da967f0c10
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-08-02 08:23:12 +0900

    feat: spring singleton configuration test

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -1,4 +1,22 @@
 package hello.core.singleton;
 
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberServiceImpl = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderServiceImpl = ac.getBean("orderService", OrderServiceImpl.class);
+        Assertions.assertThat(memberServiceImpl.getMemberRepository()).isSameAs(orderServiceImpl.getMemberRepository());
+    }
 }
