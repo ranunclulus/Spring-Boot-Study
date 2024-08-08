126eb3d1867793dd4bd0e10b40ece8000271e53a
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-08-08 16:47:58 +0900

    feat: @Bean(initMethod, destroyMethod)로 초기화 소멸화 지정

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,7 +19,7 @@ public class BeanLifeCycleTest  {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean
+        @Bean(initMethod = "connect", destroyMethod = "disConnect")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
@@ -22,15 +22,5 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     //서비스 종료시 호출
     public void disConnect() {
         System.out.println("close: " + url);
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        connect();
-        call("초기화 연결 메시지"); }
-
-    @Override
-    public void destroy() throws Exception {
-        disConnect();
     }
 }
