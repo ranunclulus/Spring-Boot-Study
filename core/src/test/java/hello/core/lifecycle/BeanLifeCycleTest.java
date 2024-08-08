bdf9ff6376be777b8256d1a88024a5ba96cabb3a
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-08-08 16:46:22 +0900

    feat: interface로 초기화 소멸화 지정

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -8,12 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 public class BeanLifeCycleTest  {
-    
+
     @Test
     public void lifeCycleTest() {
-        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
-        NetworkClient client = ac.getBean(NetworkClient.class); 
-        ac.close();
+        ConfigurableApplicationContext ac = new
+                AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); //스프링 컨테이너를 종료, ConfigurableApplicationContext 필요
     }
 
     @Configuration
@@ -21,7 +22,7 @@ public class BeanLifeCycleTest  {
         @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
-            networkClient.setUrl("http://hello-spring");
+            networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
         }
     }
