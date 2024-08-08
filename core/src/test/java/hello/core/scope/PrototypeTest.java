36ee6064efb76e947c08b09e03d3d6f79996176e
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-08-08 21:02:45 +0900

    feat: spring prototype bean

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,33 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        Assertions.assertThat(bean1).isNotSameAs(bean2);
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
