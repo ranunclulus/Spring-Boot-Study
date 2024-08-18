17801e4a8e651afe0f7aa703ac744b64d0ebfacc
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-08-19 07:26:00 +0900

    feat: order repository

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import jpabook.jpashop.domain.Order;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findeOne(Long id) {
+        return em.find(Order.class, id);
+    }
+}
