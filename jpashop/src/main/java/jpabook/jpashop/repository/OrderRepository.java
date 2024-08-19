ad90879705926f70674228f0f8c584823bcc8331
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-08-20 07:24:05 +0900

    feat: spring Order Search

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import jpabook.jpashop.domain.Order;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
@@ -16,5 +18,13 @@ public class OrderRepository {
 
     public Order findeOne(Long id) {
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m where o.status = :status and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getUsername())
+                .setMaxResults(1000)
+                .getResultList();
     }
 }
